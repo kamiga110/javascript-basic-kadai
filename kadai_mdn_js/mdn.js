c2cb40e6a7e751e8b496ab663c756126e6eeff86
kamiga110 <145092514+kamiga110@users.noreply.github.com>
2023-10-15 17:54:36 +0900

    kadai_mdnをアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+const birthday = new Date();
+const date1 = birthday.getFullYear();
+const date2 = birthday.getMonth()+1;
+const date3 = birthday.getDate();
+
+console.log(date1+'年',date2+'月',date3+'日');
+
+
